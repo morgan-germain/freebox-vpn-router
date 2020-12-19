734321b5e0dd809c790536aed81d5d956357ff94
Morgan GERMAIN LOBROT <morgan.germain@gmail.com>
2020-12-19 10:17:00 +0100

    add yunohost server

--- install-reverse-proxy.sh
+++ install-reverse-proxy.sh
@@ -1,47 +1,36 @@
 #!/bin/sh
 
 NEXTCLOUD_IP='192.168.1.4'
-CERTS_PATH='/etc/letsencrypt/live/home.morgan.netlib.re'
+YUNOHOST_IP='192.168.1.8'
+NEXTCLOUD_CERTS='/etc/letsencrypt/live/home.morgan.netlib.re'
+YUNOHOST_CERTS='/etc/letsencrypt/live/yuno.morgan.netlib.re'
 
 # Install dependencies
 sudo apt install -y nginx-light
 
 # Copy NextcloudPi certificates
 echo 'Copying NextcloudPi certificates on reverse proxy'
-sudo mkdir -p "${CERTS_PATH}"
+sudo mkdir -p "${NEXTCLOUD_CERTS}"
+sudo mkdir -p "${YUNOHOST_CERTS}"
 
-ssh "pi@${NEXTCLOUD_IP}" sudo cat "${CERTS_PATH}/fullchain.pem" | \
-    sudo tee "${CERTS_PATH}/fullchain.pem" > /dev/null
+ssh "pi@${NEXTCLOUD_IP}" sudo cat "${NEXTCLOUD_CERTS}/fullchain.pem" | \
+    sudo tee "${NEXTCLOUD_CERTS}/fullchain.pem" > /dev/null
 
-ssh "pi@${NEXTCLOUD_IP}" sudo cat "${CERTS_PATH}/privkey.pem" | \
-    sudo tee "${CERTS_PATH}/privkey.pem" > /dev/null
+ssh "pi@${NEXTCLOUD_IP}" sudo cat "${NEXTCLOUD_CERTS}/privkey.pem" | \
+    sudo tee "${NEXTCLOUD_CERTS}/privkey.pem" > /dev/null
 
 
 # Set Reverse Proxy configuration
 # https://tools.keycdn.com/http2-test
 echo 'Configuring Nextcloud reverse proxy configuration...'
-sudo tee /etc/nginx/sites-available/nextcloud > /dev/null << EOF
-server {
-	listen 443 ssl http2;
-	listen [::]:443 ssl http2;
+sudo cp ./nginx/default /etc/nginx/sites-available/default
+sudo cp ./nginx/nextcloud /etc/nginx/sites-available/nextcloud
+sudo cp ./nginx/yuno /etc/nginx/sites-available/yuno
 
-	server_name home.morgan.netlib.re;
-
-	ssl_certificate           ${CERTS_PATH}/fullchain.pem;
-	ssl_certificate_key       ${CERTS_PATH}/privkey.pem;
-
-	access_log /var/log/nginx/home-access.log;
-	error_log /var/log/nginx/home-error.log;
-
-	location / {
-		proxy_set_header Host \$host;
-		proxy_set_header X-Real-IP \$remote_addr;
-		proxy_pass http://${NEXTCLOUD_IP};
-	}
-}
-EOF
-
+sudo rm -f /etc/nginx/sites-enabled/*
+sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 sudo ln -sf /etc/nginx/sites-available/nextcloud /etc/nginx/sites-enabled/nextcloud
+sudo ln -sf /etc/nginx/sites-available/yuno /etc/nginx/sites-enabled/yuno
 sudo systemctl reload nginx
 
 echo 'You should enable HTTP ports redirection now...'
